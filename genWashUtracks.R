f6887a5e21e478e903b783b19db110a9917a65dc
Zhang Cheng <zhc268@tscc-login2.sdsc.edu>
2017-09-15 12:00:08 -0700

    change the track y fix to 0-25 scale

--- genWashUtracks.R
+++ genWashUtracks.R
@@ -7,4 +7,6 @@ track.json.simple <- gsub("\\{ \"type\":\"native_track\", \"list\":\\[ \\{ \"nam
 track.json.simple <- gsub("\\/share\\/",paste0("\\:",port,"\\/data\\/"),track.json.simple)
 track.json.simple <- gsub("pval","fc",track.json.simple)
 track.json.simple <- gsub("hammock","hammock.2",track.json.simple)# disable hammock peak track
+track.json.simple <- gsub("\"thmax\":40","\"thmax\":25",track.json.simple)# scale to 0 and 25
+track.json.simple <- gsub("\"thmin\":2","\"thmin\":0",track.json.simple)# scale to 0 and 25
 writeLines(track.json.simple,"./tracks_merged_pf.json")
