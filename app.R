95e081990627b08df3931f6646e287f155dda817
Zhang Cheng <zhc268@tscc-login2.sdsc.edu>
2017-10-04 12:05:28 -0700

    adding loading ... in the app

--- app.R
+++ app.R
@@ -1,23 +1,19 @@
 library(plotly)
 require(heatmaply)
 library(shiny)
+require(shinyjs)
 require(rbokeh)
 
 # compute a correlation matrix
 
-libs<- unlist(strsplit(readLines("./including_libs.txt"),split = " "))
-pd <- read.table("./avgOverlapFC.tab")
-pd.log2 <- log2(subset(pd,apply(pd,1,max)>2)+1)
-names(pd.log2)<- libs
-correlation <- round(cor(pd.log2,method="spearman"), 3)
 
-getXYlabel <- function(p=heatmaply(correlation,colors = Reds(n = 9),margins = c(60,100,NA,20))){
-  as.character(p$x$layout$xaxis$categoryarray)
-};
-
-xlab_correct <- getXYlabel()
 
 ui <- fluidPage(
+  useShinyjs(),
+  div(
+    id = "loading_page",
+    h1("Loading...")
+  ),
   fluidRow(
     column(7,
            plotlyOutput("heat")),
@@ -29,6 +25,20 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  
+  libs<- unlist(strsplit(readLines("./including_libs.txt"),split = " "))
+  pd <- read.table("./avgOverlapFC.tab")
+  pd.log2 <- log2(subset(pd,apply(pd,1,max)>2)+1)
+  names(pd.log2)<- libs
+  correlation <- round(cor(pd.log2,method="spearman"), 3)
+  
+  getXYlabel <- function(p=heatmaply(correlation,colors = Reds(n = 9),margins = c(60,100,NA,20))){
+    as.character(p$x$layout$xaxis$categoryarray)
+  };
+  
+  xlab_correct <- getXYlabel()
+  hide("loading_page")
+  
   output$heat <- renderPlotly({
     heatmaply(correlation,colors = Reds(n = 9),margins = c(60,100,NA,20))
    })
@@ -47,16 +57,16 @@ server <- function(input, output, session) {
     s <- event_data("plotly_click")
     if (length(s)) {
       vars <- c(s[["x"]], s[["y"]])
-
+      cat(vars)
+      cat(xlab_correct)
       x.lab<- xlab_correct[vars[2]];y.lab <- xlab_correct[vars[1]]
 
-      figure(data=pd.log2, title=paste0("cor=",correlation[x.lab,y.lab]),
+      figure(data=pd.log2, title=paste0("Spearman's cor=",correlation[x.lab,y.lab],";log2(FC+1)"),
              tools = c("pan", "wheel_zoom", "box_select",  "reset", "save")) %>%
         ly_hexbin(x.lab, y.lab, xbins = 50,trans=log2,palette="RdYlBu11")%>%
-          ly_abline(a=0,b=1)
-
+        ly_abline(a=0,b=1)
     } else {
-     figure(data=pd.log2,title=paste0("cor=",correlation[1,2]),
+     figure(data=pd.log2,title=paste0("Spearman's cor=",correlation[1,2],";log2(FC+1)"),
             tools = c("pan", "wheel_zoom", "box_select",  "reset", "save")) %>%
         ly_hexbin(libs[1], libs[2],xbins = 50,trans=log2,palette="RdYlBu11")%>%
         ly_abline(a=0,b=1)
