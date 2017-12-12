be20872d5771b7ca34d95dabf2d0a76fd5f1517b
frank@tscc <zhangc518@gmail.com>
2017-12-12 14:53:29 -0800

    fix  bug in gen sample.csv

--- libs.R
+++ libs.R
@@ -34,7 +34,7 @@ getSampleTable <- function(lib_ids){
 
     na.id <- is.na(sample_table[,3]); sample_table[na.id,3] <- sample_table[na.id,2]
     sample_table[,3] <- make.names(sample_table$`Label (for QC report)`,unique =T)
-    rownames(sample_table)<- sample_table$`Sequencing ID`
+    rownames(sample_table)<- as.character(sample_table$`Sequencing ID`)
     sample_table <- sample_table[lib_ids,] # keep same order as libs
     write.csv(file=sample_file,sample_table,row.names = F,quote=F)
     sample_table
