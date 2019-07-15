6ea136e21389582b0adf818e142d66e692539313
frank@tscc <zhangc518@gmail.com>
2019-07-15 11:25:20 -0700

    fixed to handle SE or _1. for TSS enrich

--- setQC_report_atac_chip.R
+++ setQC_report_atac_chip.R
@@ -122,9 +122,10 @@ div(class='row')
 l.tmp <- NULL;rd <- list()
 if(length(list.files(libQC_dir,paste0(libs[1],".*(.trim)*_tss-enrich.txt$")))>0){
     for(l in libs){
-        f=list.files(libQC_dir,paste0(l,"(.trim)*_tss-enrich.txt"))
+        f=list.files(libQC_dir,paste0(l,".*(.trim)*_tss-enrich.txt"))
         if(length(f)>0) {
-            if(length(f)>1) f=grep(paste0(l,"_R"),f,value=T)
+            if(length(f)>1& length(grep(paste0(l,"_R"),f,value=T))==1) f= grep(paste0(l,"_R"),f,value=T)
+            if(length(f)>1& length(grep(paste0(l,"[.]"),f,value=T))==1) f= grep(paste0(l,"."),f,value=T)
             rd[[l]] <- (read.delim(paste0(libQC_dir,f),header = F))
             l.tmp <- c(l.tmp,which(libs==l))
     }}
